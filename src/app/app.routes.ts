85bd82f44787a1501f84f4c4fe3c7a724abebc9c
Jullyo1122 <jullyoc74@gmail.com>
2025-09-27 23:20:48 -0300

    Pagina de autenticação criada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Auth } from './pages/auth/auth';
 
 export const routes: Routes = [
     {
-        path: '', component: Home
+        path: '', component: Home,
+    },
+    {
+        path: 'auth', component: Auth
     }
 ];
